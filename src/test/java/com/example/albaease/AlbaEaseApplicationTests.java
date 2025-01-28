02dc3b23e4cbd29200ad2fb465ec69b84d83340e
Jo Yu Sung <cys990617@gmail.com>
2025-01-28 22:31:20 +0900

    feat: 개발 초기환경 푸시

---
+++ src/test/java/com/example/albaease/AlbaEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.albaease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbaEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
