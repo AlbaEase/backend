60d4156520628e6a4796b7bb32c7f53810377aa9
Jo Yu Sung <cys990617@gmail.com>
2025-02-17 01:59:21 +0900

    fix: 스웨거에 제목 추가

--- src/main/java/com/example/albaease/common/config/SwaggerConfig.java
+++ src/main/java/com/example/albaease/common/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.albaease.common.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,11 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("알바이즈 API 문서")
+                        .description("알바이즈 REST API")
+                        .version("1.0.0")
+                )
                 .components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme()
                         .type(SecurityScheme.Type.HTTP)
                         .scheme("bearer")
@@ -20,4 +26,3 @@ public class SwaggerConfig {
                 .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
     }
 }
-
