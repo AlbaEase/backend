33677566acaeb10a5c0329be4e62fd8d1f433471
Jo Yu Sung <cys990617@gmail.com>
2025-02-18 19:55:31 +0900

    fix: sms컨트롤러 cors 포트번호 수정

--- src/main/java/com/example/albaease/auth/controller/AuthController.java
+++ src/main/java/com/example/albaease/auth/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*ㅎ")
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
