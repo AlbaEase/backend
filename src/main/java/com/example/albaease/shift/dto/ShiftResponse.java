c91084f0285d9827a7dc1f552817de8ef678f044
Jo Yu Sung <cys990617@gmail.com>
2025-02-18 17:26:25 +0900

    chore: 환경변수 추가

---
+++ src/main/java/com/example/albaease/shift/dto/ShiftResponse.java
@@ -0,0 +1,38 @@
+package com.example.albaease.shift.dto;
+
+import com.example.albaease.shift.domain.entity.Shift;
+import com.example.albaease.shift.domain.enums.ShiftRequestType;
+import com.example.albaease.shift.domain.enums.ShiftStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ShiftResponse {
+    private Long id;
+    private Long fromUserId;
+    private Long toUserId;
+    private Long scheduleId;
+    private Long approvedBy;
+    private ShiftRequestType requestType;
+    private ShiftStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static ShiftResponse from(Shift shift) {
+        return ShiftResponse.builder()
+                .id(shift.getRequest_id())
+                .fromUserId(shift.getFromUser().getUserId())
+                .toUserId(shift.getToUser().getUserId())
+               // .scheduleId(shift.getSchedule().getScheduleId())
+                .approvedBy(shift.getApprovedBy().getUserId())
+                .requestType(shift.getRequestType())
+                .status(shift.getStatus())
+                .createdAt(shift.getCreatedAt())
+                .updatedAt(shift.getUpdatedAt())
+                .build();
+    }
+}
