3ca5cc4a98e47eb2f187df7ad7f010988e164c87
Jo Yu Sung <cys990617@gmail.com>
2025-02-18 20:10:19 +0900

    fix: Security config 수정

--- src/main/java/com/example/albaease/common/config/SecurityConfig.java
+++ src/main/java/com/example/albaease/common/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://3.39.237.218:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // 인증정보 포함 허용
