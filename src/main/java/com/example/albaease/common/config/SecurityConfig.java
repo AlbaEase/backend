933355343d7efa1b09fab7f9a63d8c99e08b37bc
Jo Yu Sung <cys990617@gmail.com>
2025-02-21 19:05:54 +0900

    chore: reids에 세션 저장하도록 수정

--- src/main/java/com/example/albaease/common/config/SecurityConfig.java
+++ src/main/java/com/example/albaease/common/config/SecurityConfig.java
@@ -15,12 +15,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@EnableRedisHttpSession
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
